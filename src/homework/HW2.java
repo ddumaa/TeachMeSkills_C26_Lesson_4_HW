249e20ad716f9dcc5db7a756e0b272c5ed541a58
ddumaa <dimaduma62@gmail.com>
2023-11-12 17:32:45 +0300

    update 2 Big change to finding the average value in an array Now we get the real average value

--- src/homework/HW2.java
+++ src/homework/HW2.java
@@ -19,8 +19,36 @@ public class HW2 {
         }
         Arrays.sort(arrayMy);
         int lengthArray = arrayMy.length - 1;
+        System.out.println(Arrays.toString(arrayMy));
         System.out.println("largest value in the array: " + arrayMy[lengthArray]);
         System.out.println("smallest value in the array: " + arrayMy[0]);
-        System.out.println("average value in array: " + arrayMy[lengthArray / 2]);
+        //looking for the number in the center of the array
+        int middle2 = arrayMy[lengthArray / 2];
+        //looking for the position of the number
+        int position = Arrays.binarySearch(arrayMy, middle2);
+        //calculate the average between min and max
+        int middle = (arrayMy[lengthArray] - arrayMy[0]) / 2 + arrayMy[0];
+        //variable for the result
+        int middlepostionresult;
+        //loop to find the average in an array
+        for (int i = 0; i < arrayMy[lengthArray]; i++) {
+            //we check whether the real average is greater than or equal to the number in position
+            //and also less than or equal to the number in position +1
+            if (middle >= arrayMy[position] && middle <= arrayMy[position+1]){
+                //we clarify the difference in values between the average and display the nearest average
+                if ((arrayMy[position] - middle) < (arrayMy[position+1] - middle)){
+                    middlepostionresult = position;
+                }else{
+                    middlepostionresult = position + 1;
+                }
+                System.out.println("average value in array: " + arrayMy[middlepostionresult]);
+                break;
+            //change position to find the nearest average
+            } else if (middle > arrayMy[position]){
+                position++;
+            } else if (middle < arrayMy[position]) {
+                position--;
+            }
+        }
     }
 }
