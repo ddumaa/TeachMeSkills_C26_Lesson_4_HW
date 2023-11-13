cf58087d472691232ece3c1494379d469b07fe98
ddumaa <dimaduma62@gmail.com>
2023-11-13 21:05:28 +0300

    update 2

--- src/homework/HW1.java
+++ src/homework/HW1.java
@@ -10,16 +10,31 @@ public class HW1 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter a number to search and delete");
         int a = scanner.nextInt();
-        int[] numbers = {7, 8 ,15, 24, 32, 10, 48, 36};
+        int[] numbers = {7, 8, 15, 24, 8, 10, 7, 15};
+
         Arrays.sort(numbers);
-        //System.out.println(Arrays.toString(numbers));
         int out = Arrays.binarySearch(numbers, a);
+        int sum = 0;
+
         if (Math.signum(out) == -1){
             System.out.println("Number not found in array");
-        } else if (numbers[out] == a){
-            numbers[out] = 0;
-            System.out.println("Number removed");
-            //System.out.println(Arrays.toString(numbers));
+            return;
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                if(numbers[i] == a){
+                    sum++;
+                }
+            }
         }
+        int [] arrayNew = new int[numbers.length-sum];
+        int position = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != a) {
+                arrayNew[position] = numbers[i];
+                position++;
+            }
+        }
+        System.out.println(Arrays.toString(arrayNew));
     }
 }
